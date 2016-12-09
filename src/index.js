22cdf82b4c446d8ec5c474843778c6336319663c
Michael Crowley <miketcrowley@gmail.com>
2016-12-09 16:59:19 -0500

    basic set up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
 var root = document.getElementById('root');
 
-ReactDOM.render(<ul><li>Hello</li></ul>, root);
+const Header = ()=> <h1>MyApp</h1> ;
+const TodosList = ({todos})=> {
+	const lis = todos.map( (todo, idx) => <li key={idx}>{todo}</li> );
+	return (
+		<ul>
+			{ lis }
+		</ul>
+	)
+};
 
-console.log('fizz');
+class App extends Component{
+	constructor(){
+		super();
+		const todos = ['Foo', 'Bar'];
+		this.state = { todos };
+	}
+	render(){
+		return (
+			<div>
+			<Header />
+			<TodosList todos={ this.state.todos } />
+			</div>
+		)
+	}
+};
+
+ReactDOM.render(<App />, root);
