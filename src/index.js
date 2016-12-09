ec0df237f1c1d651ff5e97411fa5cd847b166df2
Michael Crowley <miketcrowley@gmail.com>
2016-12-09 16:31:09 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var root = document.getElementById('root');
+
+ReactDOM.render(<ul><li>Hello</li></ul>, root);
+
+console.log('fizz');
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					presets: ['react', 'es2015']
+				}
+			}
+		]
+	}
+};
